cdd6442e5f18771a8196baf4697098c03cccb614
yoongdoo <yoongdoo@noyungdus-MacBook-Pro.local>
2023-08-21 21:15:53 +0900

    first blockchain

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash) // hex.EncodeToString(hash[:])와 동일
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data : %s \n", block.data)
+		fmt.Printf("Hash : %s \n", block.hash)
+		fmt.Printf("Prev Hash : %s \n", block.prevHash)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+
+	chain.listBlocks()
 }
