d9984bc3449408c608294ed3cf6beb922ed816ba
yoongdoo0819 <yoongdoo0819@gmail.com>
2023-09-16 06:21:21 +0900

    Confirm Transactions

--- blockchain/block.go
+++ blockchain/block.go
@@ -58,14 +58,14 @@ func (b *Block) mine() {
 
 func createBlock(prevHash string, height int) *Block {
 	block := &Block{
-		Hash:         "",
-		PrevHash:     prevHash,
-		Height:       height,
-		Difficulty:   Blockchain().difficulty(),
-		Nonce:        0,
-		Transactions: []*Tx{makeCoinBaseTx("nico")},
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: Blockchain().difficulty(),
+		Nonce:      0,
 	}
 	block.mine()
+	block.Transactions = Mempool.TxToConfirm()
 	block.persist()
 	return block
 }
