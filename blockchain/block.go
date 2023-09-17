8571617480c0880bd0927934c0159cdf781fc049
yoongdoo0819 <yoongdoo0819@gmail.com>
2023-09-17 23:50:17 +0900

    Refactoring

--- blockchain/block.go
+++ blockchain/block.go
@@ -61,7 +61,7 @@ func createBlock(prevHash string, height int) *Block {
 		Hash:       "",
 		PrevHash:   prevHash,
 		Height:     height,
-		Difficulty: Blockchain().difficulty(),
+		Difficulty: difficulty(Blockchain()),
 		Nonce:      0,
 	}
 	block.mine()
