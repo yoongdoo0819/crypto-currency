e4454896dec22cd8c7201b51f3d0b8d0f5bfae38
yoongdoo0819 <yoongdoo0819@gmail.com>
2023-09-23 23:36:11 +0900

    Conclusion

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "github.com/nomadcoders/nomadcoin/wallet"
+import "github.com/nomadcoders/nomadcoin/cli"
 
 func main() {
-	wallet.Wallet()
+	cli.Start()
 }
--- rest/rest.go
+++ rest/rest.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/nomadcoders/nomadcoin/blockchain"
 	"github.com/nomadcoders/nomadcoin/utils"
+	"github.com/nomadcoders/nomadcoin/wallet"
 )
 
 var port string
@@ -30,6 +31,10 @@ type urlDescription struct {
 type balanceResponse struct {
 	Address string `json:"address"`
 	Balance int    `json:"balance"`
+}
+
+type myWalletResponse struct {
+	Address string `json:"addrss"`
 }
 
 func (u urlDescription) String() string {
@@ -143,6 +148,11 @@ func transactions(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusCreated)
 }
 
+func myWallet(rw http.ResponseWriter, r *http.Request) {
+	address := wallet.Wallet().Address
+	json.NewEncoder(rw).Encode(myWalletResponse{Address: address})
+}
+
 func Start(aPort int) {
 	port = fmt.Sprintf(":%d", aPort)
 
@@ -153,7 +163,8 @@ func Start(aPort int) {
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{hash:[a-f0-9]+}", block).Methods("GET")
 	router.HandleFunc("/balance/{address}", balance)
-	router.HandleFunc("/mempool", mempool)
+	router.HandleFunc("/mempool", mempool).Methods("GET")
+	router.HandleFunc("/wallet", myWallet).Methods("GET")
 	router.HandleFunc("/transactions", transactions).Methods("POST")
 
 	fmt.Printf("Listening on http://localhost%s\n", port)
