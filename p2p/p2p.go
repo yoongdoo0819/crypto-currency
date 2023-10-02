bf9b5a9c244f4f24ebe13be65050acf63c506d33
yoongdoo0819 <yoongdoo0819@gmail.com>
2023-10-02 17:36:52 +0900

    openPort

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/nomadcoders/nomadcoin/utils"
@@ -18,12 +19,16 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, "", "")
+
+	result := strings.Split(r.RemoteAddr, ":")
+	openPort := r.URL.Query().Get("openPort")
+
+	initPeer(conn, result[0], openPort)
 }
 
-func AddPeer(address, port string) {
+func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws", address, port), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
 }
