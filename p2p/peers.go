031964c822eb9e560baebac1506995c4374428bd
yoongdoo0819 <yoongdoo0819@gmail.com>
2023-10-03 14:39:02 +0900

    Messages

--- p2p/peers.go
+++ p2p/peers.go
@@ -45,11 +45,12 @@ func (p *peer) close() {
 func (p *peer) read() {
 	defer p.close()
 	for {
-		_, m, err := p.conn.ReadMessage()
+		m := Message{}
+		err := p.conn.ReadJSON(&m)
 		if err != nil {
 			break
 		}
-		fmt.Printf("%s", m)
+		fmt.Println(m.Payload)
 	}
 }
 
